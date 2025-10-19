51bdbefb9c3ea9b12e6c6a9eb214c743ed250fdb
Mizai. <shalllommm@gmail.com>
2025-10-19 13:19:08 +0700

    update koneksi Railway dan perbaikan package-lock

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
-require("dotenv").config();   // untuk membaca file .env
 const mysql = require("mysql2");
+require("dotenv").config();  // tambahkan baris ini
 
 const conn = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 conn.connect((err) => {
